160705fbeba65735be998a3868ae726342d18ae5
git.tech <ec2-user@ip-172-31-93-183.ec2.internal>
2019-10-06 22:45:15 +0000

    課題再提出

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user,only:[:update,:destroy]
   before_action :set_task,only:[:show, :edit, :update, :destroy]
   def index
     @task=current_user.tasks.build # form_with 用
@@ -7,7 +8,7 @@ class TasksController < ApplicationController
   end
 
   def show
-
+    
   end
 
   def new
@@ -54,6 +55,7 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content,:status,:user)
   end
+  
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
