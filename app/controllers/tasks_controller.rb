69dbb05446d95fea209d1b38b8cd65409d327b72
git.tech <ec2-user@ip-172-31-93-183.ec2.internal>
2019-10-07 23:20:41 +0000

    課題再提出

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user,only:[:show, :edit, :update, :destroy]
-  before_action :set_task,only:[:show, :edit, :update, :destroy]
   def index
     @task=current_user.tasks.build # form_with 用
     @tasks=current_user.tasks.order(id: :desc).page(params[:page])
@@ -46,10 +45,6 @@ class TasksController < ApplicationController
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   # Strong Parameter
   def task_params
